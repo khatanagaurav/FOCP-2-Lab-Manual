adad3a4425b065ffa5343fd97256e0181953b8e3
khatanagaurav <khatanagaurav88@gmail.com>
2026-02-07 11:24:29 +0530

    q3.c

---
+++ q3.c
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int r, area;
+    cout << "Enter radius of the fountain";
+    cin >> r;
+    cout << "radius is:" << r << "area is:" << (3.14 * r * r);
+    return 0;
+}
